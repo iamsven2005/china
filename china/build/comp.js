58829e6eed01649d0ae247b428884c36175045ca
iamsven2005 <iamsven2005@gmail.com>
2025-06-09 22:02:15 +0800

    awd

--- china/build/comp.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-(wx["webpackJsonp"] = wx["webpackJsonp"] || []).push([["comp"],{},
-/******/ function(__webpack_require__) { // webpackRuntimeModules
-/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
-/******/ __webpack_require__.O(0, ["ice"], function() { return __webpack_exec__("./node_modules/.pnpm/@ice+plugin-miniapp@1.2.3_@swc+helpers@0.5.1_react-dom@18.3.1_react@18.3.1__react@18.3.1/node_modules/@ice/plugin-miniapp/esm/miniapp/webpack/template/comp.js"); });
-/******/ var __webpack_exports__ = __webpack_require__.O();
-/******/ }
-]);
